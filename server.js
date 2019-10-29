4ec3bf4a8ec3f2b2258716e51e65c94281cfcb34
Quoc Viet <nqviet157@gmail.com>
2019-10-29 16:21:21 +0700

    saving upload files to fixed directory

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var cors = require('cors');
 const fm = require('formidable');
+const fs = require('fs');
 // require and use "multer"...
 
 var app = express();
@@ -18,14 +19,30 @@ app.get('/hello', function (req, res) {
   res.json({ greetings: "Hello, API" });
 });
 
-app.post('/api/fileanalyse', async (req, res) => {
+app.post('/api/fileanalyse', (req, res) => {
   let form = new fm.IncomingForm();
 
   form.parse(req, (err, fields, files) => {
+    if (err) throw err;
+
+    const oldPath = files.upfile.path;
+    fs.mkdir(process.cwd() + '/upload_files', { recursive: true }, (err0) => {
+      if (err0) throw err0;
+    });
+    const newPath = process.cwd() + '/upload_files/' + files.upfile.name;
+
+    fs.copyFile(oldPath, newPath, (err0) => {
+      if (err0) throw err0;
+      fs.unlink(oldPath, (err1) => {
+        if (err1) throw err1;
+        console.log('Old file is deleted.')
+      })
+    })
     res.json({
       name: files.upfile.name,
       type: files.upfile.type,
-      size: files.upfile.size
+      size: files.upfile.size,
+      path: files.upfile.path
     });
   });
 });
