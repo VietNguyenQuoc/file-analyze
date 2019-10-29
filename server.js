9fbf2354be515c59fe0a6152b771d202b049c400
Quoc Viet <nqviet157@gmail.com>
2019-10-29 14:51:21 +0700

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var cors = require('cors');
-
+const fm = require('formidable');
 // require and use "multer"...
 
 var app = express();
@@ -11,11 +11,23 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post('/api/fileanalyse', async (req, res) => {
+  let form = new fm.IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    res.json({
+      name: files.upfile.name,
+      type: files.upfile.type,
+      size: files.upfile.size
+    });
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
